deb0392c2dbd4d0784f7972c286379e3cd49fb26
Matt Lucas <mattlucas@gmail.com>
2020-05-03 11:43:39 -0400

    Update news.js
    proposed cleanup to avoid unnecessary mutation (avoid let when possible) 
    extract attributes from props
    remove logic from render methods

--- www/js/news.js
+++ www/js/news.js
@@ -1,27 +1,35 @@
 (function(){
 
-  const createArticle = (article) => (
-    `<li class="article">
-       <a href="${article.url}">
-         <img class="thumbnail" src="${article.image}"/>
+  const createArticle = (article) => {
+    
+    const { url, image, category, datetime, url, image, headline, summary, source, } = article; // extract properties 
+    const dateTime = new Date(datetime * 1000).toLocaleDateString("en-CA") // move logic outside of the rendering
+    
+    return `<li class="article">
+       <a href="${url}">
+         <img class="thumbnail" src="${image}"/>
          <div class="details">
-           <p class="category">${article.category}</p>
-           <p class="datetime">${new Date(article.datetime * 1000).toLocaleDateString("en-CA")}</p>
-           <h3 class="headline">${article.headline}</h3>
-           <p class="summary">${article.summary}</p>
-           <p class="source">${article.source}</p>
+           <p class="category">${category}</p>
+           <p class="datetime">${dateTime}</p>
+           <h3 class="headline">${headline}</h3>
+           <p class="summary">${summary}</p>
+           <p class="source">${source}</p>
          </div>
        </a>
      </li>`
-  );
+  };
 
   const getNews = async () => {
     const response = await fetch('/api/v1/news');
     const articles = await response.json();
+
     let html = "";
     articles.forEach(article => {
       html += createArticle(article);
     });
+
+    // const articleList = articles.map((article) => createArticle(article)); // get rid of 26-29 and avoid mutation
+    // return `<ul class="article-list">${articleList}</ul>`;
     return `<ul class="article-list">${html}</ul>`;
   }
 
